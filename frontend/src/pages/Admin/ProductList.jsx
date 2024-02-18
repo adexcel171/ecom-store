49865cd924bcbdc7a925d6e54b4d77c8ccbb3df0
adexcel171 <godwillokenyi@gmail.com>
2024-02-18 13:02:22 +0100

    change the lable for submit button in product list

--- frontend/src/pages/Admin/ProductList.jsx
+++ frontend/src/pages/Admin/ProductList.jsx
@@ -236,7 +236,7 @@ const ProductList = () => {
   
           <button
             onClick={handleSubmit}
-            className="py-4 px-10 mt-5 rounded-lg text-lg font-bold bg-pink-600"
+            className="py-5 px-10 mt-5 rounded-lg text-lg font-bold bg-pink-600"
           >
             Submit
           </button>
