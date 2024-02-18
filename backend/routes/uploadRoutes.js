67f10c804be9248d0ddeb351a854aad33fe6946c
adexcel171 <godwillokenyi@gmail.com>
2024-02-18 20:11:01 +0100

    final commit

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,5 +1,9 @@
 import express from 'express';
-;
+import multer from 'multer';
+import cloudinary from 'cloudinary';
+import dotenv from "dotenv";
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import mongoose from 'mongoose';
 dotenv.config();
 
 
