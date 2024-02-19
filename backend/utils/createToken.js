5d53b9fa0eeb07cd558a9dac844132fc9ae4bf82
adexcel171 <godwillokenyi@gmail.com>
2024-02-19 08:50:52 +0100

    set cookie

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: false, // Set to false during development
+    secure: true, // Set to false during development
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
