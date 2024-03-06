8d7d3a5e82b7e746612777dfdfa50e1c782bf0e2
adexcel171 <godwillokenyi@gmail.com>
2024-03-06 23:34:40 +0100

    resolved

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -8,11 +8,7 @@ const generateToken = (res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: false,
-<<<<<<< HEAD
-    secure: true, // Set to false during development
-=======
     secure: false, // Set to false during development
->>>>>>> 4543f32c7c069411fdb54d27e6f0074c077b344b
     sameSite: "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
