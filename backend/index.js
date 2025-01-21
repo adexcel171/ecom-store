08970dbb7a9c052138bc071831b069ad16b0eb42
adexcel171 <godwillokenyi@gmail.com>
2025-01-21 14:33:33 +0100

    try

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
 // Utiles
 import connectDB from "./config/db.js";
@@ -20,19 +20,22 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://excel-ecom.onrender.com',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // enable cookies
-  optionsSuccessStatus: 204,
-  allowedHeaders: 'Content-Type, Authorization'
-}));
-
+app.use(
+  cors({
+    origin: "https://excel-ecom.onrender.com/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // enable cookies
+    optionsSuccessStatus: 204,
+    allowedHeaders: "Content-Type, Authorization",
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
