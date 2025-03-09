b5936ab3d4516f88eac59d1515331b1b14db7420
adexcel171 <godwillokenyi@gmail.com>
2025-03-09 11:26:47 +0100

    updated auth to see

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -8,27 +8,25 @@ const authenticate = asyncHandler(async (req, res, next) => {
   // Read JWT from the 'jwt' cookie
   token = req.cookies.jwt;
 
+  // Log the cookies to debug if they're being received
+  console.log("Cookies received:", req.cookies);
+
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.userId).select("-password");
+      console.log("Token decoded successfully:", decoded); // Debug token decoding
       next();
     } catch (error) {
+      console.error("Token verification failed:", error.message); // Debug error
       res.status(401);
       throw new Error("Not authorized, token failed.");
     }
   } else {
+    console.log("No token found in cookies"); // Debug missing token
     res.status(401);
     throw new Error("Not authorized, no token.");
   }
 });
 
-const authorizeAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.status(401).send("Not authorized as an admin.");
-  }
-};
-
-export { authenticate, authorizeAdmin };
+export { authenticate };
